fc7fd2f363a04cb9715bd2e4979744d75f44b257
Sisodia <rajkamal.sisodia@gmail.com>
2020-07-16 20:34:37 -0400

    test5

---
+++ config.js
@@ -0,0 +1,3 @@
+// API key
+API_KEY = 'pk.eyJ1IjoicmFqc2lzb2RpYSIsImEiOiJja2FvaWZyazIwM3U4MnJwbXF4bDdtbjZoIn0.8eDEYBXvCIkxDdy_V3vDjQ'
+
---
+++ config.py
@@ -0,0 +1,3 @@
+# Postgres Usename and Password
+username = "postgres"
+pwd = "2305"
